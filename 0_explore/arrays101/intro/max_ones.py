d458d06da1af09338af2e2d6c5a258d470c1c3aa
Kenny Tan <kjtan3@dons.usfca.edu>
2022-08-27 22:45:02 -0700

    solved even digits, added max ones

--- 0_explore/arrays101/intro/max_ones.py
+++ 0_explore/arrays101/intro/max_ones.py
@@ -4,5 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
-        
+        count = 0
+        matches = 0
+        for i in nums:
+            if i == 1:
+                count += 1
+                if count >= matches:
+                    matches = count
+            else:
+                count = 0
+            
+        return matches
+
+nums = [1,0,1,1,1,0,1]
+
+ob = Solution()
+print(ob.findMaxConsecutiveOnes(nums))
