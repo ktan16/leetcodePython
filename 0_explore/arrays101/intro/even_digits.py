657f16da6431e2af731cd51385031c6a40a27da7
Kenny Tan <kjtan3@dons.usfca.edu>
2022-08-31 15:53:06 -0700

    solved even digits, added sorted squares

--- 0_explore/arrays101/intro/even_digits.py
+++ 0_explore/arrays101/intro/even_digits.py
@@ -4,4 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        matches = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                matches += 1
+        return matches
+
+nums = [5555,901,482,1771]
+
+ob = Solution()
+print(ob.findNumbers(nums))
