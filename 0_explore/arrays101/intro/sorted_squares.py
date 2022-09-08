e0be3796751ff7fc6280670d18e14e6b039ec806
Kendrick Tan <kjtan3@dons.usfca.edu>
2022-09-08 15:20:54 -0700

    remove test

--- 0_explore/arrays101/intro/sorted_squares.py
+++ 0_explore/arrays101/intro/sorted_squares.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # test
         for num in nums:
             if 0 in nums:
                 index = nums.index(0)
