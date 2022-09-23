ca057df1d038400284ee5866df1ef844954b4886
kenny tan <kjtan3@dons.usfca.edu>
2022-09-23 15:16:24 -0700

    completed sorted squares

--- 0_explore/arrays101/intro/sorted_squares.py
+++ 0_explore/arrays101/intro/sorted_squares.py
@@ -4,6 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        for num in nums:
-            if 0 in nums:
-                index = nums.index(0)
+        answer = []
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            if nums[l] * nums[l] > nums[r] * nums[r]:
+                answer.append(nums[l] * nums[l])
+                l += 1
+            else:
+                answer.append(nums[r] * nums[r])
+                r -= 1
+
+        return answer[::-1] #return reverse of array
+
+nums = [-4, -1, 0, 3, 10]
+
+ob = Solution()
+print(ob.sortedSquares(nums))
