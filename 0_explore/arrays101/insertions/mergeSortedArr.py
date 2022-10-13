98a555202efa69cffdb75040107cd5808ac5477b
kenny tan <kjtan3@dons.usfca.edu>
2022-10-13 00:11:49 -0700

    merge sorted

--- 0_explore/arrays101/insertions/mergeSortedArr.py
+++ 0_explore/arrays101/insertions/mergeSortedArr.py
@@ -7,7 +7,10 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
+        # last index of nums1
         end = m + n - 1
+
+        # merge in reverse order
         while m > 0 and n > 0:
             if nums1[m - 1] >= nums2[n - 1]:
                 nums1[end] = nums1[m - 1]
@@ -16,6 +19,11 @@ class Solution(object):
                 nums1[end] = nums2[n - 1]
                 n -= 1
             end -= 1
+        
+        while n > 0:
+            nums1[end] = nums2[n - 1]
+            n, last = n - 1, last - 1
+        
 
         
 
@@ -27,4 +35,4 @@ n = 3
 
 ob = Solution()
 ob.merge(nums1, m, nums2, n)
-print(nums1) #hello
+print(nums1)
