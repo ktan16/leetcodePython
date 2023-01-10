95f30e3ec79cd1cd6f03733f1e7c1f324927dc12
Kenny Tan <kjtan3@dons.usfca.edu>
2023-01-10 02:03:17 -0800

    inplace

--- 0_explore/arrays101/searching/check.py
+++ 0_explore/arrays101/searching/check.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
-        
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                if (arr[i] * 2 == arr[j]) or (arr[i] % 2 == 0 and arr[i] / 2 == arr[j]):
+                    return True
+        return False
+
+
+arr = [-2, 0, 10, -19, 4, 6, -8]
+
+ob1 = Solution()
+print(ob1.checkIfExist(arr))
---
+++ 0_explore/arrays101/searching/mountain.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def validMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        
+        if (len(arr) < 3):
+            return False
+        
+        i = 1
+        
+        while i < len(arr) and arr[i] > arr[i - 1]:
+            i += 1
+
+        if i == 1 or i == len(arr):
+            return False
+        
+        while i < len(arr) and arr[i] < arr[i - 1]:
+            i += 1
+
+        return i == len(arr)
+
+arr = [3, 5, 5]
+
+ob1 = Solution()
+print(ob1.validMountainArray(arr))
