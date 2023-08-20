b4d21dc66813b058bd2e753167dbb57e3de4ffbb
Kenny Tan <kjtan3@dons.usfca.edu>
2023-08-19 17:45:22 -0700

    finished contains dup

---
+++ blind_75/arrays/contains_dup.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        myset = set(nums) # convert to set; does not contain dups
+        if len(myset) == len(nums): # if len set == len list, no dups
+            return False
+        return True
+
+nums = [1,2,3,1]
+
+ob = Solution()
+print(ob.containsDuplicate(nums))
