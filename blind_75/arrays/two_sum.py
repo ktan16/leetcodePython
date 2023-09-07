b5ddeb0a11c94b5df350a7e90f15ce921be70553
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-07 02:56:11 -0700

    optimal two sum

--- blind_75/arrays/two_sum.py
+++ blind_75/arrays/two_sum.py
@@ -10,10 +10,20 @@ class Solution(object): # easy
             if x in nums and i != nums.index(x):
                 i1 = nums.index(x)
                 return [i, i1]
+            
+    def s_twoSum(self, nums, target):
+        dic = {} # key = nums[i], val = i
+        for i in range(len(nums)):
+            x = target - nums[i] # x = difference
+            if x in dic: # if difference is found in dic
+                return [dic[x], i] # return index of difference and current index
+            else:
+                dic[x] = i # else add new entry to dic
+        return
 
 nums = [3, 3]
 target = 6
 
 ob = Solution()
-print(ob.twoSum(nums, target))
+print(ob.s_twoSum(nums, target))
 
