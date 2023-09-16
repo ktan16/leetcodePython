efc421c49d34e378fa5a61f2218e459dabd485fb
kenny tan <kjtan3@dons.usfca.edu>
2023-09-15 18:33:52 -0700

    dictionary

--- 1_easy/20_valid_parentheses.py
+++ 1_easy/20_valid_parentheses.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        dic = {}
+        
         for i in range(len(s)):
             return False
         return True
