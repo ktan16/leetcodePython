2715ee062af470b3e47e68bc36d967dc5f75cc8d
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-18 00:34:04 -0700

    valid anagram solved

--- 1_easy/242_valid_anagram.py
+++ 1_easy/242_valid_anagram.py
@@ -5,7 +5,27 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        if len(t) != len(s):
+            return False
+
+        # convert s and t into dic where
+        # k = char in s/t, v = number of occurrence in string
+        s_dic = {}
+        t_dic = {}
+
+        for i in range(len(s)):
+            if s[i] in s_dic:
+                s_dic[s[i]] += 1
+            else:
+                s_dic[s[i]] = 1
+            
+            if t[i] in t_dic:
+                t_dic[t[i]] += 1
+            else:
+                t_dic[t[i]] = 1
         
+        return s_dic == t_dic # return if dictionaries are equal
+
         
 s = "anagram"
 t = "nagaram"
