29a0f35a3459ef971a7635913465e00d5a46bcef
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-19 15:47:50 -0700

    init merge two list

---
+++ 1_easy/021_merge_two_lists.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        
+
+ob = Solution()
+
+list1 = [1,2,4]
+list2 = [1,3,4]
