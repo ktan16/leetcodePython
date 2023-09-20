63042413b4f906c96135badb4d32618f2d39e509
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-19 20:35:54 -0700

    347 solved

--- 2_medium/347_top_k_frequent.py
+++ 2_medium/347_top_k_frequent.py
@@ -5,7 +5,29 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # create dictionary to store key = num, val = occurrence
+        counts = {}
+        for i in range(len(nums)):
+            if nums[i] in counts:
+                counts[nums[i]] += 1
+            else:
+                counts[nums[i]] = 1
+
+        # sort dictionary by each item's value in descending order
+        # {2: 7, 1: 6, 3: 8} -> {3: 8, 2: 7, 1: 6}
+        counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        
+        # return k_arr with top k elements
+        k_arr = []
+        for i in range(k):
+            k_arr.append(counts[i][0])
+
+        return k_arr
+            
 
 ob = Solution()
 
-nums = [1,1,1,2,2,3]
+nums = [2,2,3,1,1,1]
+k = 2
+
+print(ob.topKFrequent(nums, k))
