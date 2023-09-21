5576da365c1f3b55fde7ee68ae2431849db1cacc
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-20 17:45:27 -0700

    035 solved

---
+++ 1_easy/035_search_insert_position.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if nums[len(nums) - 1] < target:
+            return len(nums)
+        
+        for i in range(len(nums)):
+            if nums[i] == target or nums[i] > target:
+                return i
+
+            
+
+ob = Solution()
+
+nums = [1,3,5,6]
+target = 7
+
+print(ob.searchInsert(nums, target))
