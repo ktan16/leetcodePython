68ddd376dbfea592d184e4e875b74fb0a3f8f411
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-21 00:57:07 -0700

    495 solved

--- 1_easy/495_teemo_attacking.py
+++ 1_easy/495_teemo_attacking.py
@@ -5,4 +5,17 @@ class Solution(object):
         :type duration: int
         :rtype: int
         """
+        poisoned = 0
+        prev_time = 0
+
+        for i in range(len(timeSeries) - 1):
+            poisoned += min(timeSeries[i + 1] - timeSeries[i], duration)
         
+        return poisoned + duration
+
+ob = Solution()
+
+timeSeries = [1, 4, 5]
+duration = 2
+
+print(ob.findPoisonedDuration(timeSeries, duration))
