b0d29cf8e2e0235dbeae18b855974d9a1cb77c1a
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-24 00:04:47 -0700

    comments

--- 1_easy/495_teemo_attacking.py
+++ 1_easy/495_teemo_attacking.py
@@ -6,8 +6,8 @@ class Solution(object):
         :rtype: int
         """
         poisoned = 0
-        prev_time = 0
 
+        # if poison overlap duration, pick the lesser number to add
         for i in range(len(timeSeries) - 1):
             poisoned += min(timeSeries[i + 1] - timeSeries[i], duration)
         
