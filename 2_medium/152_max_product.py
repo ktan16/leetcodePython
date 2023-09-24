64844649f79bf33f4f876ee12145ecefb02f919c
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-24 03:59:25 -0700

    152 edit

--- 2_medium/152_max_product.py
+++ 2_medium/152_max_product.py
@@ -4,4 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        mp = 0
+        start = 0
+        end = 0
+        p = nums[end]
+
+        while end < len(nums):
+            
+            if p >= mp:
+                mp = p
+                end += 1
+                p *= nums[end]
+            elif p < mp and end - start > 1:
+                p /= nums[start]
+                start += 1
+            elif p < mp and end - start == 1:
+                end += 1
+                p *= nums[end]
+                
+        return mp
+
+ob = Solution()
+
+nums = [2,3,-2,4]
+print(ob.maxProduct(nums))
