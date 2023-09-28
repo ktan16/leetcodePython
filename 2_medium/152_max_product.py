d9167d2691cf6224d679c2197e355130cd07aee9
Kenny Tan <kjtan3@dons.usfca.edu>
2023-09-28 00:37:05 -0700

    152 solved

--- 2_medium/152_max_product.py
+++ 2_medium/152_max_product.py
@@ -4,24 +4,53 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        mp = max(nums)
+        currMin, currMax = 1,1
+
+        for num in nums:
+            tmp = currMax * num # temp var to prevent min calc error
+            currMax = max(num * currMax, num * currMin, num)
+            currMin = min(tmp, num * currMin, num)
+
+            mp = max(mp, currMax)
+        
+        return mp
+
+    def m_maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        if len(nums) == 1:
+            return nums[0]
+        
         mp = 0
+        p = 1
         start = 0
         end = 0
-        p = nums[end]
 
-        while end < len(nums):
+        # sliding window technique
+        # while start <= end and end < len(nums):
+        while True:
+            if end != len(nums) - 1:
+                p *= nums[end]
             
-            if p >= mp:
+            if p > mp:
                 mp = p
                 end += 1
-                p *= nums[end]
-            elif p < mp and end - start > 1:
-                p /= nums[start]
-                start += 1
-            elif p < mp and end - start == 1:
-                end += 1
-                p *= nums[end]
-                
+            
+            else:
+                if end != len(nums) - 1:
+                    end += 1
+                else:
+                    if nums[start] != 0:
+                        p /= nums[start]
+                    start += 1
+
+            if start == end:
+                break
+
         return mp
 
 ob = Solution()
