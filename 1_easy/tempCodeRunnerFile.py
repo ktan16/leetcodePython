d4d8c0f9fc36fe1340e5534895616c469f3fb663
Kenny Tan <kjtan3@dons.usfca.edu>
2023-10-02 18:17:27 -0700

    test

--- 1_easy/tempCodeRunnerFile.py
+++ 1_easy/tempCodeRunnerFile.py
@@ -1,31 +1,4 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        strs.sort()
-        first = strs[0]
-        last = strs[-1]
-        
-        if len(strs) < 1:
-            return ""
-        
-        if first[0] != last [0]:
-            return ""
-        
-        lcp = ""
-
-        for i in range(len(first)):
-            if first[i] == last[i]:
-                lcp += first[i]
-            else:
-                break
-        
-        return lcp
-
-    
-ob = Solution()
-
-strs = ["a"]
-print(ob.longestCommonPrefix(strs))
+curr = head
+while curr:
+    print(curr.val)
+    curr = curr.next
--- 4_test/test.py
+++ 4_test/test.py
@@ -1,9 +1,50 @@
-arr = [4,7,1,4]
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-for i in range(len(arr) - 1):
-    first = arr[i]
-    second = arr[i + 1]
+class Solution(object):
+    def list_to_linked_list(self, lst):
+        """
+        Converts a regular list to a singly-linked list.
+        :type lst: List[int]
+        :rtype: ListNode
+        """
+        if not lst:
+            return None
 
-    mid = (first + second)//2
-    print(mid)
+        head = ListNode(lst[0])
+        current = head
 
+        for val in lst[1:]:
+            current.next = ListNode(val)
+            current = current.next
+
+        return head
+
+    def reverseList(self, head): # iterative
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev = None
+        curr = head
+
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
+
+ob = Solution()
+
+nums = [1,2,3,4,5]
+head = ob.list_to_linked_list(nums)
+
+curr = head
+
+while curr:
+    print(curr.val)
+    curr = curr.next
