8b8f73733368fc828ca34ed6eb568bc26eb0fa3d
Kenny Tan <kjtan3@dons.usfca.edu>
2023-10-17 14:03:49 -0700

    021 solved

--- 1_easy/021_merge_two_lists.py
+++ 1_easy/021_merge_two_lists.py
@@ -1,8 +1,9 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
         """
@@ -10,6 +11,21 @@ class Solution(object):
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        merged = ListNode() # create new head of linked list
+        curr = merged
+
+        while list1 and list2: # iterate through both lists
+            if list1.val < list2.val: # if l1 < l2, add l1 to curr
+                curr.next = list1
+                list1 = list1.next # iterate l1
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next # iterate curr
+
+        curr.next = list1 if list1 else list2 # add remainder
+
+        return merged.next # return head.next as new head
         
 
 ob = Solution()
