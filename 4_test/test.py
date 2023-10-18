d943983ef71cc05f971d11f2e83831f522cf2f1b
Kenny Tan <kjtan3@dons.usfca.edu>
2023-10-18 01:24:48 -0700

    info image test

--- 4_test/test.py
+++ 4_test/test.py
@@ -1,50 +1,7 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+def usage_count(ladder):
+  # Write your code here
+  min_integrity = min(ladder)
+  return min_integrity - 1
 
-class Solution(object):
-    def list_to_linked_list(self, lst):
-        """
-        Converts a regular list to a singly-linked list.
-        :type lst: List[int]
-        :rtype: ListNode
-        """
-        if not lst:
-            return None
-
-        head = ListNode(lst[0])
-        current = head
-
-        for val in lst[1:]:
-            current.next = ListNode(val)
-            current = current.next
-
-        return head
-
-    def reverseList(self, head): # iterative
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        prev = None
-        curr = head
-
-        while curr:
-            tmp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = tmp
-        return prev
-
-ob = Solution()
-
-nums = [1,2,3,4,5]
-head = ob.list_to_linked_list(nums)
-
-curr = head
-
-while curr:
-    print(curr.val)
-    curr = curr.next
+ladder = [4, 5, 5, 4, 3, 5, 4]
+print(usage_count(ladder)) # Should print 2
