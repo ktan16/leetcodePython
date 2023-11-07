e89781ea3a82d3f7cbbd5b1b6a6883b14cc36adf
Kenny Tan <kjtan3@dons.usfca.edu>
2023-11-06 23:36:24 -0800

    322 solved

--- 4_test/test.py
+++ 4_test/test.py
@@ -1,28 +1,8 @@
-def theFinalProblem(target):
-    # Write your code here
-	minimumFlips = 0
-	zero = '0'
-	one = '1'
-
-	start = [0] * len(target)
-
-
-	for i in range(len(target)):
-		if int(target[i]) != start[i]:
-			if target[i] == one:
-				start[i] = 1
-			elif target[i] == zero:
-				start[i] = 0
-
-			minimumFlips += 1
-
-			for k in range(i + 1, len(target)):
-				if start[k] == 0:
-					start[k] = 1
-				elif start[k] == 1:
-					start[k] = 0
-
-	return minimumFlips
-
-target = "0011"
-print(theFinalProblem(target))
+def add(n):
+	if n == 0:
+		return 0
+	
+	else:
+		return n + add(n - 1)
+	
+print(add(5))
