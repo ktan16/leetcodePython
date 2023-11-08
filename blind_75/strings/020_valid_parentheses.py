839108b1c81693892b63d14a6173f91920685e26
Kenny Tan <kjtan3@dons.usfca.edu>
2023-11-08 00:20:34 -0800

    020 solved

---
+++ blind_75/strings/020_valid_parentheses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {'}':'{', ')':'(', ']':'['}
+
+        for paren in s:
+
+            # if this is a closing paren
+            if paren in pairs:
+                # if the stack is not empty and the most recently added is the pair
+                if stack and stack[-1] == pairs[paren]:
+                    # remove the matching open paren from stack
+                    stack.pop()
+                else:
+                    # stack is empty or most recent paren is not the opening pair
+                    return False
+                
+            # only add open parens
+            else:
+                stack.append(paren)
+
+        return True
+
+s = "([][))"
+
+ob = Solution()
+print(ob.isValid(s))
